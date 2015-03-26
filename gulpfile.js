d9f550ff1b9dec19079d94011673ad86c0acb931
Tim Douglas <neurotech@gmail.com>
2015-03-26 16:09:59 +1100

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+
+var assets = {
+  stylus: [ 'src/stylus/*.styl' ],
+  js: [ 'src/js/*.js' ]
+};
+
+// SASS
+gulp.task('stylus', function () {
+  return gulp.src(assets.stylus)
+    .pipe(stylus({ compress: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/css'));
+});
+
+// JS
+gulp.task('js', function() {
+  return gulp.src(assets.js)
+    .pipe(gulp.dest('build/js'))
+});
+
+// Clean
+gulp.task('clean', function() {
+  return gulp.src(['public/css/**', 'public/js/**'], {read: false})
+    .pipe(clean());
+});
+
+// Build
+gulp.task('build', ['clean'], function() {
+  gulp.start('stylus', 'js');
+});
+
+// Watch
+gulp.task('watch', ['clean', 'build'], function() {
+  gulp.watch('src/stylus/**/*.styl', ['sass']);
+  gulp.watch('src/js/**/*.js', ['js']);
+});
