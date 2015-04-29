c156914e62d344a35fe2fabbbd2e8b610084d15b
Tim Douglas <neurotech@gmail.com>
2015-04-29 20:08:56 +1000

    Added Marks language change and fadein to Learning Resources navbar item

--- src/js/rosebank.js
+++ src/js/rosebank.js
@@ -1,14 +1,26 @@
 // Grab the course ID portion of the current URL
-var path = window.location.pathname.split('/')[2];
+var path = window.location.pathname.split('/');
 
 // URL to custom CSS file for hiding the sidebar
 var sidebar = 'https://s3-ap-southeast-2.amazonaws.com/rosebank-canvas/css/sidebar.min.css';
 
 // ID 187 = Learning Resources
-if (path === '187') {
+if (path[2] === '187') {
   $('head').append('<link rel="stylesheet" href="' + sidebar + '" type="text/css" media="all" />');
 }
 
 // Add Learning Resources link to navbar
-$('ul#menu').append('<li id="extra_menu_item" class="menu-item"><a href="/courses/187" class="menu-item-no-drop">Learning Resources</a></li>');
+$('<li id="extra_menu_item" class="menu-item"><a href="/courses/187" class="menu-item-no-drop">Learning Resources</a></li>').hide().appendTo("ul#menu").fadeIn(300);
 
+// Change any reference to 'Marks' to 'Results & Feedback'
+var marksFor = $('div#content[role="main"] h2').first().text().split(' ');
+
+$('li a.grades').text('Results & Feedback');
+$('nav#breadcrumbs ul li a[href*="/grades"]').first().text('Results & Feedback');
+$('li#grades_menu_item a').text('Results & Feedback');
+$('table td.percent span').text('no results');
+$('a.print-grades').text('Print results');
+
+if (path[3] === 'grades') {
+  $('div#content[role="main"] h2').text('Results ' + marksFor[1] + ' ' + marksFor[5] + ' ' + marksFor[6]);
+}
