5c11cc08a63ab4d1e6a8cb9e41f96e0050d12c28
Tim Douglas <neurotech@gmail.com>
2015-05-27 10:33:09 +1000

    Added a simple logging of currently logged in user's role
    Tidied up readability of jQuery code that makes use of multiple chained
    methods

--- src/js/rosebank.js
+++ src/js/rosebank.js
@@ -4,16 +4,26 @@ var path = window.location.pathname.split('/');
 // URL to custom CSS file for hiding the sidebar
 var sidebar = 'https://s3-ap-southeast-2.amazonaws.com/rosebank-canvas/css/sidebar.min.css';
 
+// Log the currently logged in user's role data
+console.log('Current role data: ' + ENV.current_user_roles);
+
 // ID 187 = Learning Resources
 if (path[2] === '187') {
-  $('head').append('<link rel="stylesheet" href="' + sidebar + '" type="text/css" media="all" />');
+  $('head')
+    .append('<link rel="stylesheet" href="' + sidebar + '" type="text/css" media="all" />');
 }
 
 // Add Learning Resources link to navbar
-$('<li id="extra_menu_item" class="menu-item"><a href="/courses/187" class="menu-item-no-drop">Learning Resources</a></li>').hide().appendTo("ul#menu").fadeIn(300);
+$('<li id="extra_menu_item" class="menu-item"><a href="/courses/187" class="menu-item-no-drop">Learning Resources</a></li>')
+  .hide()
+  .appendTo("ul#menu")
+  .fadeIn(300);
 
 // Change any reference to 'Marks' to 'Results & Feedback'
-var marksFor = $('div#content[role="main"] h2').first().text().split(' ');
+var marksFor = $('div#content[role="main"] h2')
+  .first()
+  .text()
+  .split(' ');
 
 $('li a.grades').text('Results & Feedback');
 $('nav#breadcrumbs ul li a[href*="/grades"]').first().text('Results & Feedback');
